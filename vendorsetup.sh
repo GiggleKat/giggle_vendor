a69089e46f0d8763f7c63b8a7cd0176e58fb0b6c
Patrick <gigglekatrom@gmail.com>
2014-11-04 02:18:35 +0100

    Changed up paths and boots

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,16 @@ for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build
 do
     add_lunch_combo $combo
 done
+
+add_lunch_combo gigglekat_vs980-userdebug
+add_lunch_combo gigglekat_moto_msm8960-userdebug
+add_lunch_combo gigglekat_ghost-userdebug
+add_lunch_combo gigglekat_hammerhead-userdebug
+add_lunch_combo gigglekat_d801-userdebug
+add_lunch_combo gigglekat_d802-userdebug
+add_lunch_combo gigglekat_m7-userdebug
+add_lunch_combo gigglekat_m8-userdebug
+add_lunch_combo gigglekat_d801-userdebug
+add_lunch_combo gigglekat_d800-userdebug
+add_lunch_combo gigglekat_falcon-userdebug
+add_lunch_combo gigglekat_yuga-userdebug
